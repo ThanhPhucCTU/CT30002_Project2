8a11acf64d68d768823c706a98f0da15b914e981
ThanhPhucCTU <phucB1910433@student.ctu.edu.vn>
2023-02-15 04:21:51 +0700

    modified hello.java and deleted hello.class

---
+++ hello.java
@@ -0,0 +1,8 @@
+class hello{
+    public static void main(String []args) {
+        String msg1 = "Hello";
+        String msg2 = "world";
+        System.out.println(msg1);
+        System.out.println(msg2);
+    }
+}
